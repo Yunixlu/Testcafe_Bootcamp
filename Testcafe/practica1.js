f6832da7690638ed9313372f05a3f8fa511b4c95
Yunuen <yunuen.pl@gmail.com>
2020-10-15 21:36:26 -0500

    Primeras practicas

---
+++ Testcafe/practica1.js
@@ -0,0 +1,9 @@
+import { Selector} from 'testcafe';
+
+fixture('Practica 1 con testcafe')
+    .page('https://the-internet.herokuapp.com/');
+
+//Hacer una sincronización en la página con mi elemento t
+test('Probar abrir una página', async t => {
+
+});
---
+++ Testcafe/practica2.js
@@ -0,0 +1,10 @@
+import { Selector } from 'testcafe';
+import page from './pageModel';
+
+fixture('Practica 2 con testcafe')
+    .page('https://the-internet.herokuapp.com/');
+
+//Hacer una sincronización en la página con mi elemento t
+test('Probar dar clic en un enlace', async t => {
+
+});
